5aafc79ddee6ce7cb662fe96d81371280a88c33e
braedonsaunders <bsaunders@rassaun.com>
2026-04-23 09:42:55 -0400

    Make plugin execution declarative

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -4,6 +4,7 @@ export * from "./mock-data";
 export * from "./quote-engine";
 export * from "./dataset-utils";
 export * from "./plugin-calculators";
+export * from "./plugin-output-templates";
 export * from "./first-party-plugins";
 export * from "./summary-builder";
 export * from "./worksheet-hours";
